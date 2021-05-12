f339b28d4c2014df028da141fd9768db015ea35c
Lalitha-choudhary <79392683+Lalitha-choudhary@users.noreply.github.com>
2021-05-12 17:24:38 +0530

    Add files via upload

---
+++ portfolio/home.js
@@ -0,0 +1,27 @@
+ 
+ function changetoabout(){
+     var hello=document.getElementById("hello").innerText="ABOUT";
+ }
+
+ 
+ function changetohello(){
+    var hello=document.getElementById("hello").innerText="HELLO";
+}
+
+function changetoproject(){
+    var iam=document.getElementById("iam").innerText="PROJECT";
+}
+
+
+function changetoiam(){
+   var iam=document.getElementById("iam").innerText="I AM";
+}
+
+function changetocontact(){
+    var lalitha=document.getElementById("lalitha").innerText="CONTACT";
+}
+
+
+function changetolalitha(){
+   var lalitha=document.getElementById("lalitha").innerText="LALITHA";
+}
